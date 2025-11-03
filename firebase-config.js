44ef183b990f8020947e157b40e59cac511ea7e4
Albert Lahat <lahat.albert@gmail.com>
2025-11-03 18:09:26 -0500

    Initial commit

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase config for RPS game
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-it9Fler4HlYiriRb5F661wL5UMs5X1Y",
+  authDomain: "rpsgame-1e5fb.firebaseapp.com",
+  projectId: "rpsgame-1e5fb",
+  storageBucket: "rpsgame-1e5fb.firebasestorage.app",
+  messagingSenderId: "876435062726",
+  appId: "1:876435062726:web:47d43b51d47368009cbcc7",
+  measurementId: "G-E6ESPD4SYW",
+  // Ensure RTDB writes go to the correct instance
+  databaseURL: "https://rpsgame-1e5fb-default-rtdb.firebaseio.com"
+};
